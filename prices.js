3f45fda1d544876c41c01cb80fed6b0fcd9ebe50
Chrome2112 <ChromeGaming2112@gmail.com>
2015-08-22 14:38:37 -0400

    mikeadded
    yes

---
+++ prices.js
@@ -0,0 +1,14 @@
+
+ $("#ChromeDonation").hover(function() {
+
+ 		$(this).css("text-decoration", "underline");
+
+ });
+
+  $("#ChromeDonation").hoveroff(function() {
+
+ 		$(this).css("text-decoration", "none");
+
+ });
+
+
